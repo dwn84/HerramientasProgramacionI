3326108a6ea2d430d10b4dbb7f25787df4bd6060
dwn84 <50523550+dwn84@users.noreply.github.com>
2020-03-06 15:23:38 -0500

    Ejemplo creación de clases y objetos

---
+++ EjemploClases/EjemploClases/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploClases
+{//definir la clase 
+    class BankAccount
+    {
+        //definir las variables
+        private string nombre;
+        private int numeroCuenta;
+        private double saldo;
+        //definir atributos: click derecho en el nombre de la variable
+        //Acciones rápidas y refactorización
+        //encapsular campo y usar propiedad.
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int NumeroCuenta { get => numeroCuenta; set => numeroCuenta = value; }
+        public double Saldo { get => saldo; set => saldo = value; }
+        //definir los métodos o acciones
+        //escribir la firma del método
+        // modificar de acceso: private - public
+        // tipo de retorno: void - int -string -...
+        // nombre del método: verbo
+        // parentesis para los parámetros de entrada.
+        public void Depositar(double dinero) {
+            //implementación del método
+            saldo += dinero;
+        }
+
+        public void Retirar(double dinero)
+        {
+            //validación de un error en el método
+            if (saldo < dinero) {
+                //generamos una nueva excepción
+                throw new Exception("Saldo insuficiente");
+            }
+            saldo -= dinero;
+        }
+
+        public string MostrarDetalles() 
+        {
+            string mensaje = "La cuenta #" + numeroCuenta
+                + " pertenece a " + nombre
+                + " tiene un saldo de: " + saldo;
+            
+            //al final o en los condicionales retornamos el resultado
+            return mensaje;
+        }
+
+    }
+}
---
+++ EjemploClases/EjemploClases/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EjemploClases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear o instanciar el objeto
+            BankAccount Annie = new BankAccount();
+            //modifican las propiedades
+            Annie.Nombre = "Annie Dickinson";
+            Annie.NumeroCuenta = 100010002;
+            Annie.Saldo = 350000;
+            //llamar o invocar las acciones/métodos
+            Console.WriteLine(Annie.MostrarDetalles());
+            Console.WriteLine("Ingrese el dinero a depositar");
+            double dinerito = Convert.ToDouble(Console.ReadLine());
+            Annie.Depositar(dinerito);
+            Console.WriteLine(Annie.MostrarDetalles());
+            Console.WriteLine("Ingrese el dinero a retirar");
+            double money = Convert.ToDouble(Console.ReadLine());
+            try
+            {
+                Annie.Retirar(money);
+            }
+            catch (Exception err) {
+                Console.WriteLine(err.Message);            
+            }
+            Console.WriteLine(Annie.MostrarDetalles());
+        }
+    }
+}
